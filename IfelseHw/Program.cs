098c361e48a58710c55aaef8405ccb55eda36bfb
chanchan <chiayaa841203@gmail.com>
2023-12-26 10:57:11 +0800

    ifelseHw

--- IfelseHw/Program.cs
+++ IfelseHw/Program.cs
@@ -10,6 +10,500 @@ namespace IfelseHw
     {
         static void Main(string[] args)
         {
+            //兩個數x和y，選出大的那一個
+            //Console.WriteLine("請輸入x");
+            //double x = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("請輸入y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //if (x > y)
+            //{
+            //    Console.WriteLine($"最大值是x{x}");
+            //}
+            //else {
+            //    Console.WriteLine($"最大值是y{y}");
+            //}
+            //Console.ReadKey();
+
+            //x和y，如果x>y，則回傳x-y，否則回傳y-x
+            //Console.WriteLine("請輸入x");
+            //double x = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("請輸入y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //double z ;
+
+            //if (x > y)
+            //{
+            //    z = (x - y);
+            //    Console.WriteLine(z);
+            //}
+            //else {
+            //    z = (y - x);
+            //    Console.WriteLine(z);
+            //}
+            //Console.WriteLine($"值{z}");
+            //Console.ReadKey();
+
+            //是負數，就將它轉成正數
+            //Console.WriteLine("請輸入x");
+            //double x = Convert.ToDouble(Console.ReadLine());
+
+            //if (x < 0)
+            //{
+            //    x = Math.Abs(x);
+            //    Console.WriteLine($"值{x}");
+            //}
+            //else {
+            //    Console.WriteLine($"值{x}");
+            //}
+            //Console.ReadKey();
+
+            //學生的分數，輸出該成績級別
+            //Console.WriteLine("請輸入成績");
+            //double x = Convert.ToDouble(Console.ReadLine());
+
+            //if (x >= 90)
+            //{
+            //    Console.WriteLine("grade=\"A\"");
+            //}
+            //else if (x >= 80)
+            //{
+            //    Console.WriteLine("grade=\"B\"");
+            //}
+            //else if (x >= 70)
+            //{
+            //    Console.WriteLine("grade=\"C\"");
+            //}
+            //else if (x >= 60)
+            //{
+            //    Console.WriteLine("grade=\"D\"");
+            //}
+            //else {
+            //    Console.WriteLine("grade=\"F\"");
+            //}
+            //Console.ReadKey();
+
+            //switch改寫
+            //Console.WriteLine("請輸入成績");
+            //double x = Convert.ToDouble(Console.ReadLine());
+
+            //switch (x/10)
+            //{
+            //    case 10:
+            //        Console.WriteLine("grade=\"A\"");
+            //        break;
+
+            //    case 9:
+            //        Console.WriteLine("grade=\"B\"");
+            //        break;
+
+            //    case 8:
+            //        Console.WriteLine("grade=\"C\"");
+            //        break;
+
+            //    case 7:
+            //        Console.WriteLine("grade=\"D\"");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("grade=\"F\"");
+            //        break;
+            //}
+            //Console.ReadKey();
+
+            //折扣
+            //Console.WriteLine("請輸入Money");
+            //double x = Convert.ToDouble(Console.ReadLine());
+
+            //double y;
+            //if (x < 1000)
+            //{
+            //    y = Math.Round(x * 1.0);
+            //    Console.WriteLine($"無折扣，價格是{y}");
+            //}
+            //else if (x <= 4999)
+            //{
+            //    y = Math.Round(x * 0.9);
+            //    Console.WriteLine($"9折折扣，價格是{y}");
+            //}
+            //else if (x <= 9999)
+            //{
+            //    y = Math.Round(x * 0.8);
+            //    Console.WriteLine($"8折折扣，價格是{y}");
+            //}
+            //else {
+            //    y = Math.Round(x * 0.7);
+            //    Console.WriteLine($"7折折扣，價格是{y}");
+            //}
+            //Console.ReadKey();
+
+            //輸入x和y，如果x>=y，則列印x，否則列印y
+            //Console.WriteLine("請輸入x");
+            //double x = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("請輸入y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //if (x >= y)
+            //{
+            //    Console.WriteLine($"z = {x}");
+            //}
+            //else {
+            //    Console.WriteLine($"z = {y}");
+            //}
+            //Console.ReadKey();
+
+            //x和y，如果x和y都是正數，令z=1，如兩者均為負數，令z=-1，否則令z=0
+            //Console.WriteLine("請輸入x");
+            //double x = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("請輸入y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //if (x > 0 && y > 0)
+            //{
+            //    Console.WriteLine("z=1");
+            //}
+            //else if (x <= 0 && y <= 0)
+            //{
+            //    Console.WriteLine("z=-1");
+            //}
+            //else {
+            //    Console.WriteLine("z=0");
+            //}
+            //Console.ReadKey();
+
+            //輸入x、y、u、v，如果(x+y)>(u+v)，則令z=x-y，否則令z=u-v
+
+            //Console.WriteLine("請輸入x");
+            //double x = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("請輸入y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("請輸入u");
+            //double u = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("請輸入v");
+            //double v = Convert.ToDouble(Console.ReadLine());
+
+            //double z;
+            //if ((x + y) > (u + v))
+            //{
+            //    z = x + y;
+            //    Console.WriteLine($"{z} = x + y");
+            //}
+            //else {
+            //    z = u + v;
+            //    Console.WriteLine($"{z} =u + v");
+            //}
+            //Console.ReadKey();
+
+
+            ////輸入x、y、u、v，如果(x + y) / (u + v)>=2，則令z = x - y，否則令z = u - v
+
+            //Console.WriteLine("請輸入x");
+            //double x = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("請輸入y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("請輸入u");
+            //double u = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("請輸入v");
+            //double v = Convert.ToDouble(Console.ReadLine());
+
+            //double z;
+            //if ((x + y) / (u + v) > 2)
+            //{
+            //    z = x - y;
+            //    Console.WriteLine($"z = x - y 值是 {z}");
+            //}
+            //else if ((x + y) / (u + v) == 2)
+            //{
+            //    z = x - y;
+            //    Console.WriteLine($"z = x - y 值是 {z}");
+            //}
+            //else {
+            //    z = u - v ;
+            //    Console.WriteLine($"z = u - v 值是 {z}");
+            //}
+            //Console.ReadKey();
+
+            ////輸入x、y，如果 x >=y，則令 z = x^2 ，否則令 z = y^2
+            ////Math.Pow
+
+            //Console.WriteLine("請輸入x");
+            //double x = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("請輸入y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //double z;
+            //if (x > y)
+            //{
+            //    z = Math.Pow(x,2);
+            //    Console.WriteLine($"z = x^2  值是 {z}");
+            //}
+            //else if (x == y)
+            //{
+            //    z = Math.Pow(x, 2);
+            //    Console.WriteLine($"z = x^2  值是 {z}");
+            //}
+            //else
+            //{
+            //    z = Math.Pow(y, 2);
+            //    Console.WriteLine($"z = y^2 值是 {z}");
+            //}
+            //Console.ReadKey();
+
+            ////income收入tax計算
+            //Console.WriteLine("請輸入income收入");
+            //double x = Convert.ToDouble(Console.ReadLine());
+
+            //double y;
+            //if (x > 4090000)
+            //{
+            //    y = (x * 0.4) - 721100;
+            //    Console.WriteLine($"tax 是 {y}");
+            //}
+            //else if (x > 2180000)
+            //{
+            //    y = (x * 0.3) - 312100;
+            //    Console.WriteLine($"tax 是 {y}");
+            //}
+            //else if (x > 1090000)
+            //{
+            //    y = (x * 0.21) - 115900;
+            //    Console.WriteLine($"tax 是 {y}");
+            //}
+            //else if (x > 410000)
+            //{
+            //    y = (x * 0.13) - 28700;
+            //    Console.WriteLine($"tax 是 {y}");
+            //}
+            //else { 
+            //    y = (x * 0.06);
+            //    Console.WriteLine($"tax 是 {y}");
+            //}
+            //Console.ReadKey();
+
+            //// 2-7
+            //Console.WriteLine("請輸入x");
+            //double x = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("請輸入y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //if (x > 0)
+            //{
+            //    if (y > 0)
+            //    {
+            //        Console.WriteLine("1st quadrant\n");
+            //    }
+            //    else if (y == 0)
+            //    {
+            //        Console.WriteLine("X-axis\n");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("4th quadran\n");
+            //    }
+            //}
+            //else if (x == 0)
+            //{
+            //    if (y == 0)
+            //    {
+            //        Console.WriteLine("Origin\n");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Y-axis\n");
+            //    }
+            //}
+            //else {
+            //    if (y > 0)
+            //    {
+            //        Console.WriteLine("2nd quadran\n");
+            //    }
+            //    else {
+            //        if (y == 0)
+            //        {
+            //            Console.WriteLine("X-axis\n");
+            //        }
+            //        else {
+            //            Console.WriteLine("3th quadran\n");
+            //        }
+            //    }
+            //}
+            //Console.ReadKey();
+
+            ////補充習題 1
+            //Console.WriteLine("請輸入題數");
+            //int x = Convert.ToInt32(Console.ReadLine());
+
+            //int y;
+            //if (x > 40)
+            //{
+            //    Console.WriteLine("幾霸婚!");
+            //}
+            //else if (x <= 40 && x >= 21)
+            //{
+            //    y = 60 + (x - 10) * 1;
+            //    Console.WriteLine($"{y}分");
+
+            //}
+            //else if (x <= 20 && x >= 11)
+            //{
+            //    y = 60 + (x - 10) * 2;
+            //    Console.WriteLine($"{y}分");
+            //}
+            //else{
+            //    y = x * 6;
+            //    Console.WriteLine($"{y}分");
+            //}
+            //Console.ReadKey();
+
+            //BMI
+            //double a, b, c;
+
+            //Console.WriteLine("請輸入您的身高(公尺)");
+            //a = Convert.ToDouble(Console.ReadLine());
+
+
+            //Console.WriteLine("請輸入您的體重");
+            //b = Convert.ToDouble(Console.ReadLine());
+
+            //c = b / Math.Pow(a, 2);
+
+            //if (c >= 35)
+            //{
+            //    Console.WriteLine("阿喔~過重囉");
+            //}
+            //else if (c < 35 && c >= 30)
+            //{
+            //    Console.WriteLine("目前中度肥胖，再接再厲就會過重囉");
+            //}
+            //else if (c < 30 && c >= 27)
+            //{
+            //    Console.WriteLine("目前輕度肥胖");
+
+            //}
+            //else if (c < 27 && c >= 24)
+            //{
+            //    Console.WriteLine("目前過重");
+            //}
+            //else if (c < 24 && c >= 18.5)
+            //{
+            //    Console.WriteLine("目前正常範圍");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("體重過輕");
+            //}
+            //Console.ReadKey();
+
+            //隨意輸入三個數，比大小
+            //Console.WriteLine("請輸入x");
+            //double x = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("請輸入y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("請輸入z");
+            //double z = Convert.ToDouble(Console.ReadLine());
+
+            //if (x > y && x > z )
+            //{
+            //    if (y > z)
+            //    {
+            //        Console.WriteLine($"{x}、{y}、{z}");
+            //    }
+            //    else {
+            //        Console.WriteLine($"{x}、{z}、{y}");
+            //    }
+            //}
+            //else if (y > x && y > z)
+            //{
+            //    if (x > z) {
+            //        Console.WriteLine($"{y}、{x}、{z}");
+            //    } else{
+            //        Console.WriteLine($"{y}、{z}、{x}");
+            //    }
+            //}
+            //else
+            //{
+            //    if (x > y) {
+            //        Console.WriteLine($"{z}、{x}、{y}");
+            //    }else
+            //        Console.WriteLine($"{z}、{y}、{x}");
+            //}
+            //Console.ReadKey();
+
+            //隨意輸入三個數，比大小
+            //Sort
+            //Console.WriteLine("請輸入x");
+            //double x = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("請輸入y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("請輸入z");
+            //double z = Convert.ToDouble(Console.ReadLine());
+
+            //double[] myNumbers = { x, y, z };
+            //Array.Sort(myNumbers);
+            //Array.Reverse(myNumbers);
+            //foreach (double i in myNumbers)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadKey();
+
+
+
+
+            //獎金利潤
+            Console.WriteLine("請輸入I");
+            double I = Convert.ToDouble(Console.ReadLine());
+
+            double pay;
+            if (I >= 1000000)
+            {
+                pay = I * 0.01 + (I - 600000) * 0.015 + (I - 400000) * 0.03 + (I - 200000) * 0.05;
+
+                Console.WriteLine($"獎金為{pay}");
+            }
+            else if (600000 < I && I <= 1000000)
+            {
+                pay = I * 0.05;
+                Console.WriteLine($"獎金為{pay}");
+            }
+            else if (400000 < I && I <= 600000)
+            {
+                pay = I * 0.03;
+                Console.WriteLine($"獎金為{pay}");
+            }
+            else if (200000 < I && I <= 400000)
+            {
+                pay = I * 0.015;
+                Console.WriteLine($"獎金為{pay}");
+            }
+            else if (I >= 100000 && I < 200000)
+            {
+                pay = I * 0.075;
+                Console.WriteLine($"獎金為{pay}");
+            }
+            else
+            {
+                pay = I * 0.01;
+                Console.WriteLine($"獎金為{pay}");
+            }
+            Console.ReadKey();
         }
     }
 }
